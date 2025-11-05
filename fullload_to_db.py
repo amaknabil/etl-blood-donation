45709ce30fed07e805fbd0ca46609e06b688b788
amaknabil <156818354+amaknabil@users.noreply.github.com>
2025-11-05 14:55:48 +0800

    add validation

--- fullload_to_db.py
+++ fullload_to_db.py
@@ -12,10 +12,13 @@ donnorrate_url = os.getenv("RATE_PARQUET_URL")
 db_path = os.getenv("DUCKDB_FILE_PATH") 
 
 def load_data_to_db(con,url,table):
-    query = f"CREATE OR REPLACE TABLE {table} AS SELECT * FROM read_parquet('{url}')"
-    print(f"Starting to loading data into {table} table")
-    con.execute(query)
-    print(f"Successfully loaded data into {table} histocial\n")
+    try:
+        query = f"CREATE OR REPLACE TABLE {table} AS SELECT * FROM read_parquet('{url}')"
+        print(f"Starting to loading data into {table} table")
+        con.execute(query)
+        print(f"Successfully loaded data into {table} histocial\n")
+    except Exception as e:
+        print(f"Fail to full load on table {table} because : {e}")
 
 try:
     print(f"Connecting to {db_path}")
