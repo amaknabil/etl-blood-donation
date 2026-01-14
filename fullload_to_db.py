f4cf18e27f6f71c123958921b6caeefe83488aea
amaknabil <amaknabil2001@gmail.com>
2026-01-14 11:18:59 +0800

    add population func

--- fullload_to_db.py
+++ fullload_to_db.py
@@ -9,6 +9,7 @@ load_dotenv(env_path, override=True)
 historical_url = os.getenv("COMPLETE_PARQUET_URL")
 retention_url = os.getenv("RETENTION_PARQUET_URL")
 donnorrate_url = os.getenv("RATE_PARQUET_URL")
+population_url = os.getenv("POPULATION_URL")
 
 db_path = os.getenv("DUCKDB_FILE_PATH") 
 
@@ -90,6 +91,17 @@ def load_transformed_donorrate_to_db(con,url,table):
         except Exception as e:
             print(f"Fail to full load on table {table} because : {e}")
 
+def load_population_table(con,url,table):
+        try:
+            query = f""" create or replace table {table}  as select * from read_parquet('{url}') """
+            print(f"Starting to loading data into table {table}")
+            con.execute(query)
+            print(f"Successfully loaded data into table {table} \n")
+        except Exception as e:
+            print(f"Fail to full load on table {table} because : {e}")
+
+
+
 
 try:
     print(f"Connecting to {db_path}")
@@ -97,10 +109,10 @@ try:
     print(f"Successfully connected to {db_path}\n")
     add_table(con,'inst_code.csv','inst_code')
     add_table(con,'race.csv','race')
-    print(historical_url)
     load_transformed_historical_to_db(con,historical_url,'historical')
     load_transformed_retention_to_db(con,retention_url,'retention')
     load_transformed_donorrate_to_db(con,donnorrate_url,"donorrate")
+    load_population_table(con,population_url,'population')
     
     query = "CREATE or replace TABLE malaysia_states AS SELECT * FROM ST_Read('https://github.com/wmgeolab/geoBoundaries/raw/9469f09/releaseData/gbOpen/MYS/ADM1/geoBoundaries-MYS-ADM1_simplified.geojson')"
     print("Adding Malaysia_states table")
